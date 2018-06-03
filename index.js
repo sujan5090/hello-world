6c5b4c3d2f77e99416a8a81a716ff4d0cb7a6937
sujan5090 <sujan.chirumamilla@gmail.com>
2018-06-03 18:25:35 +0530

    server_setup for project

---
+++ index.js
@@ -0,0 +1,32 @@
+const express=require("express");
+const app=express();
+const mongoose=require("mongoose");
+const config=require("./config/database");
+const path=require("path");
+
+mongoose.promise=global.promise;
+
+mongoose.connect(config.uri, (err) => {
+ 
+    if(err)
+    {
+       console.log("could not connet to the database ",err);
+    }
+    else
+    {
+     
+      console.log("connect to the database "+config.db);
+    }
+
+});
+app.use(express.static(__dirname+'/client/dist/client/'));
+
+app.get('*', (req, res) =>{
+    res.sendFile(path.join(__dirname +'/client/dist/client/index.html'));
+  });
+
+
+app.listen(8080,() =>{
+
+    console.log("listening on the port 8080");
+  });
